891f47fc84a31c4d4a7b4f4e3871d28a0f93f407
eranga_mdw <erangamdw@gmail.com>
2022-07-14 02:24:57 +0530

    find object class

---
+++ reflection03.java
@@ -0,0 +1,12 @@
+public class Reflection03 {
+
+    public static void main(String[] args) {
+
+        Primary p = new Primary();
+        System.out.println("class = " + p.getClass());
+        System.out.println("class name = " + p.getClass().getName());
+    }
+}
+
+
+
