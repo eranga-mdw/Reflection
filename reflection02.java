67de8121ce163dcf68c7f29897dd6d714cff72b9
eranga_mdw <erangamdw@gmail.com>
2022-07-14 02:24:15 +0530

    access private members

---
+++ reflection02.java
@@ -0,0 +1,17 @@
+public class Reflection02 {
+
+    public static void main(String[] args) {
+        
+        Primary p = new Primary();
+
+        p.powerX();
+        p.powerY();
+
+        double x = p.getX;
+        double y = p.getY
+
+        System.out.println("p = " + p);
+}
+
+
+
